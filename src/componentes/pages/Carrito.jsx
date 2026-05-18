8cabd7f4afa5300305ceba0078d904360c14c38e
Fabian Gomez <cursosfabiangomez@gmail.com>
2026-05-18 09:30:57 -0300

    Finalizado revision carrito y pagina de compras

--- src/componentes/pages/Carrito.jsx
+++ src/componentes/pages/Carrito.jsx
@@ -1,9 +1,25 @@
+import { useCart } from "../../context/CartContext.jsx";
+
 function Carrito() {
+  const { carrito } = useCart();
+
   return (
     <div className="carrito">
       <h1>Carrito de compras</h1>
 
-      <p>No hay productos agregados todavía.</p>
+      {carrito.length === 0 ? (
+        <p>No hay productos agregados todavía.</p>
+      ) : (
+        carrito.map((producto) => (
+          <div key={producto.id} className="producto-carrito">
+            <h2>{producto.nombre}</h2>
+
+            <p>Precio: ${producto.precio}</p>
+
+            <p>Cantidad: {producto.cantidad}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
